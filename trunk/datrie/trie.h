58d5453bc7fc0388e6ff6b83801904098b7a10e3
thep <thep@1ef183b1-9dc7-4fb3-affb-44359c0196fe>
2006-08-12 15:58:06 +0000

    This commit was generated by cvs2svn to compensate for changes in r2, which included commits to RCS files with non-trunk default branches.

---
+++ trunk/datrie/trie.h
@@ -0,0 +1,192 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * trie.h - Trie data type and functions
+ * Created: 2006-08-11
+ * Author:  Theppitak Karoonboonyanan <thep@linux.thai.net>
+ */
+
+#ifndef __TRIE_H
+#define __TRIE_H
+
+#include "triedefs.h"
+
+/**
+ * @file trie.h
+ * @brief Trie data type and functions
+ */
+
+/**
+ * @brief Trie data type
+ */
+typedef struct _Trie   Trie;
+
+/**
+ * @brief Trie walking state
+ */
+typedef struct _TrieState TrieState;
+
+/*-----------------------*
+ *   GENERAL FUNCTIONS   *
+ *-----------------------*/
+
+/**
+ * @brief Open a trie
+ *
+ * @param path : the path that stores the trie files
+ * @param name : the name of the trie (not actual file name)
+ * @param mode : openning mode, read or write
+ *
+ * @return a pointer to the openned trie, NULL on failure
+ *
+ * Open a trie of given name. Note that @a name here does not mean the
+ * actual file name. Rather, the file name will be inferred by the name.
+ */
+Trie *  trie_open (const char *path, const char *name, TrieIOMode mode);
+
+/**
+ * @brief Close a trie
+ *
+ * @param trie: the trie
+ *
+ * @return 0 on success, non-zero on failure
+ *
+ * Close the given trie. If @a trie was openned for writing, all pending
+ * changes will be saved to file.
+ */
+int     trie_close (Trie *trie);
+
+/**
+ * @brief Save a trie
+ *
+ * @param trie: the trie
+ *
+ * @return 0 on success, non-zero on failure
+ *
+ * If @a trie was openned for writing, save all pending data to file.
+ */
+int     trie_save (Trie *trie);
+
+
+/*------------------------------*
+ *   GENERAL QUERY OPERATIONS   *
+ *------------------------------*/
+
+/**
+ * @brief Retrieve an entry from trie
+ *
+ * @param trie   : the trie
+ * @param key    : the key for the entry to retrieve
+ * @param o_data : the storage for storing the entry data on return
+ *
+ * @return boolean value indicating the existence of the entry.
+ *
+ * Retrieve an entry for the given @a key from @a trie. On return,
+ * if @a key is found and @a o_val is not NULL, @a *o_val is set
+ * to the data associated to @a key.
+ */
+Bool    trie_retrieve (Trie *trie, const TrieChar *key, TrieData *o_data);
+
+/**
+ * @brief Store a value for  an entry to trie
+ *
+ * @param trie  : the trie
+ * @param key   : the key for the entry to retrieve
+ * @param data  : the data associated to the entry
+ *
+ * @return boolean value indicating the success of the process
+ *
+ * Store a data @a data for the given @a key in @a trie. If @a key does not 
+ * exist in @a trie, it will be appended.
+ */
+Bool    trie_store (Trie *trie, const TrieChar *key, TrieData data);
+
+
+/*-------------------------------*
+ *   STEPWISE QUERY OPERATIONS   *
+ *-------------------------------*/
+
+/**
+ * @brief Get root state of a trie
+ *
+ * @param trie : the trie
+ *
+ * @return the root state of the trie
+ *
+ * Get root state of @a trie, for stepwise walking.
+ *
+ * The returned state is allocated and must be freed with trie_state_free()
+ */
+TrieState * trie_root (Trie *trie);
+
+
+/*----------------*
+ *   TRIE STATE   *
+ *----------------*/
+
+/**
+ * @brief Clone a trie state
+ *
+ * @param s    : the state to clone
+ *
+ * @return an duplicated instance of @a s
+ *
+ * Make a copy of trie state.
+ *
+ * The returned state is allocated and must be freed with trie_state_free()
+ */
+TrieState * trie_state_clone (const TrieState *s);
+
+/**
+ * @brief Free a trie state
+ *
+ * @param s    : the state to free
+ *
+ * Free the trie state.
+ *
+ * The returned state is allocated and must be freed with trie_state_free()
+ */
+void      trie_state_free (TrieState *s);
+
+/**
+ * @brief Walk the trie from the state
+ *
+ * @param s    : current state
+ * @param c    : key character for walking
+ *
+ * @return boolean value indicating the success of the walk
+ *
+ * Walk the trie stepwise, using a given character @a c.
+ * On return, the state @a s is updated to the new state if successfully walked.
+ */
+Bool      trie_state_walk (TrieState *s, const TrieChar c);
+
+/**
+ * @breif Check for leaf state
+ *
+ * @param s    : the state to check
+ *
+ * @return boolean value indicating whether it is a leaf state
+ *
+ * Check if the given state is a leaf state. A leaf state is a trie state 
+ * that terminates a key, and stores a value associated with the key.
+ */
+Bool      trie_state_is_leaf (const TrieState *s);
+
+/**
+ * @brief Get data from leaf state
+ *
+ * @param s    : the leaf state
+ *
+ * @return the data associated with the leaf state @a s,
+ *         or TRIE_DATA_ERROR if @a s is not a leaf state
+ *
+ * Get value from a leaf state of trie. Getting the value from leaf state 
+ * will result in TRIE_DATA_ERROR.
+ */
+TrieData trie_state_get_data (const TrieState *s);
+
+#endif  /* __TRIE_H */
+
+/*
+vi:ts=4:ai:expandtab
+*/
