99b4202379a1778946ab35445ea3b21dcb74a489
thep-svn <theppitak@gmail.com>
2009-04-03 08:57:16 +0000

    2009-04-03  Theppitak Karoonboonyanan  <thep@linux.thai.net>
    	* datrie/fileutils.h, datrie/fileutils.c
    	(make_full_path, file_open, file_length): Remove unused codes.
    	* datrie/triedefs.h (TrieIOMode): Remove unused typedef.

--- datrie/fileutils.c
+++ datrie/fileutils.c
@@ -10,67 +10,11 @@
 
 #include "fileutils.h"
 
-/*--------------------------------------*
- *    INTERNAL FUNCTIONS DECLARATIONS   *
- *--------------------------------------*/
-
-static char *   make_full_path (const char *dir,
-                                const char *name,
-                                const char *ext);
-
 /* ==================== BEGIN IMPLEMENTATION PART ====================  */
 
 /*--------------------------------*
  *    FUNCTIONS IMPLEMENTATIONS   *
  *--------------------------------*/
-
-static char *
-make_full_path (const char *dir, const char *name, const char *ext)
-{
-    char   *path;
-
-    path = (char *) malloc (strlen (dir) + strlen (name) + strlen (ext) + 2);
-    sprintf (path, "%s/%s%s", dir, name, ext);
-
-    return path;
-}
-
-FILE *
-file_open (const char *dir, const char *name, const char *ext, TrieIOMode mode)
-{
-    const char *std_mode;
-    char       *full_path;
-    FILE       *file;
-
-    if (mode & TRIE_IO_WRITE)
-        std_mode = "r+";
-    else
-        std_mode = "r";
-
-    full_path = make_full_path (dir, name, ext);
-    file = fopen (full_path, std_mode);
-    if (!file && mode & TRIE_IO_CREATE)
-        file = fopen (full_path, "w+");
-    free (full_path);
-
-    return file;
-}
-
-long
-file_length (FILE *file)
-{
-    long    cur_pos;
-    long    size;
-
-    cur_pos = ftell (file);
-
-    fseek (file, 0L, SEEK_END);
-    size = ftell (file);
-
-    fseek (file, cur_pos, SEEK_SET);
-
-    return size;
-}
 
 Bool
 file_read_int32 (FILE *file, int32 *o_val)
