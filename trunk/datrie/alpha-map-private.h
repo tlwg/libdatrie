d3838b3a58da7e56ad64655e874160169898dd54
thep-svn <thep-svn@1ef183b1-9dc7-4fb3-affb-44359c0196fe>
2010-02-24 09:47:17 +0000

    * datrie/*.h, datrie/*.c: Add license header to every source file.

--- trunk/datrie/alpha-map-private.h
+++ trunk/datrie/alpha-map-private.h
@@ -1,5 +1,24 @@
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
+ * libdatrie - Double-Array Trie Library
+ * Copyright (C) 2006  Theppitak Karoonboonyanan
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/*
  * alpha-map-private.h - private APIs for alpha-map
  * Created: 2008-12-04
  * Author:  Theppitak Karoonboonyanan <thep@linux.thai.net>
--- trunk/datrie/alpha-map.c
+++ trunk/datrie/alpha-map.c
@@ -1,5 +1,24 @@
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
+ * libdatrie - Double-Array Trie Library
+ * Copyright (C) 2006  Theppitak Karoonboonyanan
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/*
  * alpha-map.c - map between character codes and trie alphabet
  * Created: 2006-08-19
  * Author:  Theppitak Karoonboonyanan <thep@linux.thai.net>
