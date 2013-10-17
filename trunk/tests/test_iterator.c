57dc668b8c45a78512e303c8f94fcf41ffd1e842
thep-svn <thep-svn@1ef183b1-9dc7-4fb3-affb-44359c0196fe>
2013-10-17 07:12:07 +0000

    Write a test for trie iterator.
    * tests/test_iterator.c:
      - Write test suite for trie iterator.

--- trunk/tests/test_iterator.c
+++ trunk/tests/test_iterator.c
@@ -31,19 +31,102 @@
 int
 main ()
 {
-    Trie *test_trie;
+    Trie         *test_trie;
+    DictRec      *dict_p;
+    TrieState    *trie_root_state;
+    TrieIterator *trie_it;
+    Bool          is_failed;
 
+    msg_step ("Preparing trie");
     test_trie = en_trie_new ();
     if (!test_trie) {
         fprintf (stderr, "Fail to create test trie\n");
-        return 1;
+        goto err_trie_not_created;
     }
 
     /* store */
-    /* iterate & check */
+    msg_step ("Adding data to trie");
+    for (dict_p = dict_src; dict_p->key; dict_p++) {
+        if (!trie_store (test_trie, dict_p->key, dict_p->data)) {
+            printf ("Failed to add key '%ls', data %d.\n",
+                    dict_p->key, dict_p->data);
+            goto err_trie_created;
+        }
+    }
 
+    /* iterate & check */
+    msg_step ("Iterating and checking trie contents");
+    trie_root_state = trie_root (test_trie);
+    if (!trie_root_state) {
+        printf ("Failed to get trie root state\n");
+        goto err_trie_created;
+    }
+    trie_it = trie_iterator_new (trie_root_state);
+    if (!trie_it) {
+        printf ("Failed to get trie iterator\n");
+        goto err_trie_root_created;
+    }
+
+    is_failed = FALSE;
+    while (trie_iterator_next (trie_it)) {
+        AlphaChar *key;
+        TrieData   key_data, src_data;
+
+        key = trie_iterator_get_key (trie_it);
+        if (!key) {
+            printf ("Failed to get key from trie iterator\n");
+            is_failed = TRUE;
+        }
+        key_data = trie_iterator_get_data (trie_it);
+        if (TRIE_DATA_ERROR == key_data) {
+            printf ("Failed to get data from trie iterator for key '%ls'\n",
+                    key);
+            is_failed = TRUE;
+        }
+        /* mark entries found in file */
+        src_data = dict_src_get_data (key);
+        if (TRIE_DATA_ERROR == src_data) {
+            printf ("Extra entry in file: key '%ls', data %d.\n",
+                    key, key_data);
+            is_failed = TRUE;
+        } else if (src_data != key_data) {
+            printf ("Data mismatch for: key '%ls', expected %d, got %d.\n",
+                    key, src_data, key_data);
+            is_failed = TRUE;
+        } else {
+            dict_src_set_data (key, TRIE_DATA_READ);
+        }
+
+        free (key);
+    }
+
+    /* check for unmarked entries, (i.e. missed in file) */
+    for (dict_p = dict_src; dict_p->key; dict_p++) {
+        if (dict_p->data != TRIE_DATA_READ) {
+            printf ("Entry missed in file: key '%ls', data %d.\n",
+                    dict_p->key, dict_p->data);
+            is_failed = TRUE;
+        }
+    }
+
+    if (is_failed) {
+        printf ("Errors found in trie iteration.\n");
+        goto err_trie_it_created;
+    }
+
+    trie_iterator_free (trie_it);
+    trie_state_free (trie_root_state);
     trie_free (test_trie);
     return 0;
+
+err_trie_it_created:
+    trie_iterator_free (trie_it);
+err_trie_root_created:
+    trie_state_free (trie_root_state);
+err_trie_created:
+    trie_free (test_trie);
+err_trie_not_created:
+    return 1;
 }
 
 /*
