cca2a286e1d8efafae0f32916e1478ab85096bb8
Stephen L Arnold <nerdboy@gentoo.org>
2021-09-20 23:18:52 -0700

    new: dev: add tox test runner, clang toolchain file, .gitignore
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

---
+++ clang_toolchain.cmake
@@ -0,0 +1,7 @@
+SET (CMAKE_ASM_COMPILER "clang")
+SET (CMAKE_ASM-ATT_COMPILER "clang")
+SET (CMAKE_C_COMPILER "clang")
+SET (CMAKE_CXX_COMPILER "clang++")
+SET (CMAKE_Fortran_COMPILER "gfortran")
+SET (CMAKE_AR ar CACHE FILEPATH "Archive manager" FORCE)
+SET (CMAKE_RANLIB ranlib CACHE FILEPATH "Archive index generator" FORCE)
